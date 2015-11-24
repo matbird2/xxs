fe6caa7596513fadd146a67198efd23a752fdccd
matbird <matbird@126.com>
2015-11-24 16:30:08 +0800

    修改CommentActivity布局

--- app/src/main/java/com/xxs/leon/xxs/activity/CommentActivity.java
+++ app/src/main/java/com/xxs/leon/xxs/activity/CommentActivity.java
@@ -3,7 +3,10 @@ package com.xxs.leon.xxs.activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -29,9 +32,9 @@ public class CommentActivity extends AppCompatActivity{
     @InstanceState
     protected Bundle savedInstanceState;
     @ViewById
-    protected ImageView logo;
+    protected FloatingActionButton fab;
     @ViewById
-    protected ImageView logo_in;
+    protected Toolbar toolbar;
 
     IconicsDrawable okIcon;
 
@@ -44,8 +47,8 @@ public class CommentActivity extends AppCompatActivity{
 
     @AfterViews
     void initViews(){
-//        logo_in.setImageDrawable(okIcon);
-        exitTransition = ActivityTransition.with(getIntent()).to(logo).start(savedInstanceState);
+        initToolbar();
+        exitTransition = ActivityTransition.with(getIntent()).to(fab).start(savedInstanceState);
     }
 
     void initIconRes(){
@@ -53,6 +56,21 @@ public class CommentActivity extends AppCompatActivity{
                 .icon(CommunityMaterial.Icon.cmd_check)
                 .color(Color.WHITE)
                 .sizeDp(20);
+    }
+
+    private void initToolbar(){
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+
+            final ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setDisplayShowHomeEnabled(true);
+                actionBar.setDisplayShowTitleEnabled(true);
+                actionBar.setDisplayUseLogoEnabled(false);
+                actionBar.setHomeButtonEnabled(true);
+            }
+        }
     }
 
     @Override
