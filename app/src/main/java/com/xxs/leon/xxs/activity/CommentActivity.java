89c5a1b9462d6bcf2593601b4c5807654a9afbed
matbird <matbird@126.com>
2015-11-25 08:59:35 +0800

    x

--- app/src/main/java/com/xxs/leon/xxs/activity/CommentActivity.java
+++ app/src/main/java/com/xxs/leon/xxs/activity/CommentActivity.java
@@ -94,7 +94,7 @@ public class CommentActivity extends AppCompatActivity{
     @Click(R.id.fab)
     void testRest(){
         client.setRestErrorHandler(errorHandler);
-        
+
 //        testCloud();
         testGetNewAlbums();
     }
