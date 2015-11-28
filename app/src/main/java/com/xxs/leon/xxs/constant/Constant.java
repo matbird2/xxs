2791771969988e40b5f4ce0fe0fc9a71d6fd05b6
matbird2 <maliang531@126.com>
2015-11-28 11:49:21 +0800

    xx

--- app/src/main/java/com/xxs/leon/xxs/constant/Constant.java
+++ app/src/main/java/com/xxs/leon/xxs/constant/Constant.java
@@ -5,8 +5,11 @@ package com.xxs.leon.xxs.constant;
  */
 public interface Constant {
 
-    String X_BMOB_APPLICATION_ID = "";
-    String X_BMOB_REST_API_KEY = "";
+    String X_BMOB_APPLICATION_ID = "9e441b998f89368cc102c01306fa92ae";
+    String X_BMOB_REST_API_KEY = "a17a32506201029ffb7d0bb4921f0e45";
+
+//    String X_BMOB_APPLICATION_ID = "";
+//    String X_BMOB_REST_API_KEY = "";
 
     int LOG_LEVEL = 6;
     String AES_ENCRYPT_PASSWORD = "xxs";
